1c57bdd667e388aaa081716c6a87aaa88bb175a4
unknown <stephenoluwafemi52@gmail.comm>
2023-09-15 09:09:21 +0100

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,23 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+import React from "react";
+import NavBar from './NavBar'; // Import the NavBar component
+import Home from './Home'; // Import the Home component
+import About from './About'; // Import the About component
+
+function App() {
+  return (
+    <div>
+      <NavBar /> {/* Include the NavBar component */}
+      <Home /> {/* Include the Home component */}
+      <About /> {/* Include the About component */}
+    </div>
+  );
+}
+
+export default App;
+
 
 
 function App() {
